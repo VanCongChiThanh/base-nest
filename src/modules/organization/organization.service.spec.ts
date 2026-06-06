51db3eb44e633878fffc67f8bf35a32d2e98efb8
Văn Công Chí Thanh <vancongchithanh2004@gmail.com>
2026-06-06 15:40:52 +0700

    test: enhance organization controller and service tests with necessary providers

--- src/modules/organization/organization.service.spec.ts
+++ src/modules/organization/organization.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrganizationService } from './organization.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Job, JobApplication } from '../job/entities';
+import { PaymentConfirmation, Escrow } from '../payment/entities';
 
 describe('OrganizationService', () => {
   let service: OrganizationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrganizationService],
+      providers: [
+        OrganizationService,
+        {
+          provide: getRepositoryToken(Job),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(JobApplication),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(PaymentConfirmation),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Escrow),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<OrganizationService>(OrganizationService);
